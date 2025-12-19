bac5767cdfbbfce5537bb87b713db33a49eba2cc
AsadFahimTEC <fah485434@gmail.com>
2025-12-19 20:43:25 +0600

    procedure example added properly

--- procedures.sql
+++ procedures.sql
@@ -10,14 +10,22 @@ $$
 
 call delete_emp_byid(4)
 
-create procedure increase_low_salary(department_name VARCHAR(50))
+create procedure increase_low_salary_test(department_name VARCHAR(50))
 language plpgsql
 as
 $$
   declare
   avg_salary int;
   begin
-  select avg(salary) from employee
-  where department = department_name
+  -- first step
+  select avg(salary) into avg_salary from employee
+  where department = department_name;
+
+  -- second step
+  update employee set salary = salary * 1.1
+  where department = department_name and salary < avg_salary;
+  
   end;
 $$
+
+call increase_low_salary_test('IT')
